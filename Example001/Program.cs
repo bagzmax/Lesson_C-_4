81728f13adaa1ad45018c6f493491b23f7ca3076
Maksim <bagzmax75@gmail.com>
2022-07-03 10:27:17 +0300

    add Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,17 @@
+﻿// 7 - сумма всех чисел
+
+int GetSumm(int a)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        sum +=i;
+    }
+    return sum; 
+}
+System.Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(GetSumm(num));
+
