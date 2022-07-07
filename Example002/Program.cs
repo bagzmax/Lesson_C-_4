45deed033a9272c311f986e26e9b4d39241a3129
Maksim <bagzmax75@gmail.com>
2022-07-07 23:48:29 +0300

    add example 002

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -3,20 +3,26 @@
 // 78 -> 2
 // 89126 -> 5
 
-Console.Write("Введите целое число: ");
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (; num!=0; num/=10)
+{
+    count++;
+}
+System.Console.WriteLine("Количество цифр в числе: " + (count));
 
 //Console.WriteLine((int)Math.Log10(num) + 1);
 
 // Задано число N, найти произведение числел от 1 до N
 
-int factorial(int num)
-{
-    int result = 1;
-    for (int i = 1; i <= num; i++)
-    {
-        result *=i;
-    }
-    return result;
-}
-System.Console.WriteLine(factorial(num));
+//int factorial(int num)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         result *=i;
+//     }
+//     return result;
+// }
+// System.Console.WriteLine(factorial(num));
